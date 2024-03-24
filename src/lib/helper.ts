817744da4a083cec9f6dea634d14e4f14031bd9e
Ashmit <sashmit2003@gmail.com>
2024-03-24 21:12:53 +0530

    Fix: Vercel Redirect

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,5 +1,10 @@
+
+
+
 export const getURL = () => {
   // let url = import.meta.env.BACKEND_URL ?? "http://localhost:3000"; // Default URL for development.
   let url ="https://nest-api-tp8n.onrender.com"  
   return url;
 };
+
+
