7962e42509f32de1bcde19f9e351d2458b08e6b9
ashmit-zen <ashmit@zensible.in>
2025-06-22 19:07:42 +0530

    Update: Change Backend URL for API Integration

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,5 +1,5 @@
 export const getURL = () => {
   // let url = import.meta.env.BACKEND_URL ?? "http://localhost:3000"; // Default URL for development.
-  const url = "http://localhost:3000";
+  let url = "https://nest-api-tp8n.onrender.com";
   return url;
 };
